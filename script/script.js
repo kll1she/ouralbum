78780cbf031bbf58a56e5994a7040c9ce888d6cb
kll1she <91278891+kll1she@users.noreply.github.com>
2023-02-18 00:27:35 +0600

    Add files via upload

---
+++ script/script.js
@@ -0,0 +1,62 @@
+// Сортировка
+var q1 = document.querySelector('#q1');
+var q2 = document.querySelector('#q2');
+var flex = document.querySelector('.flex');
+
+q1.onclick = function () {
+	if (flex.classList.contains('sort-function')) {
+		flex.classList.remove('sort-function');
+	}
+}
+
+q2.onclick = function () {
+	flex.classList.add('sort-function');
+}
+
+// Кнопка фильтр
+var filter = document.querySelector('.filter');
+var sort_button = document.querySelector('.sort');
+
+filter.onclick = function () {
+	sort_button.classList.toggle('hidden');
+}
+
+// Таймер
+document.addEventListener('DOMContentLoaded', function() {
+	// конечная дата, например 1 июля 2021
+	let deadline = new Date('Oct 11 2023 00:00:00');
+	// id таймера
+	let timerId = null;
+	// склонение числительных
+	function declensionNum(num, words) {
+	  return words[(num % 100 > 4 && num % 100 < 20) ? 2 : [2, 0, 1, 1, 1, 2][(num % 10 < 5) ? num % 10 : 5]];
+	}
+	// вычисляем разницу дат и устанавливаем оставшееся времени в качестве содержимого элементов
+	function countdownTimer() {
+	  const diff = deadline - new Date();
+	  if (diff <= 0) {
+		clearInterval(timerId);
+	  }
+	  const days = diff > 0 ? Math.floor(diff / 1000 / 60 / 60 / 24) : 0;
+	  const hours = diff > 0 ? Math.floor(diff / 1000 / 60 / 60) % 24 : 0;
+	  const minutes = diff > 0 ? Math.floor(diff / 1000 / 60) % 60 : 0;
+	  const seconds = diff > 0 ? Math.floor(diff / 1000) % 60 : 0;
+	  $days.textContent = days < 10 ? '0' + days : days;
+	  $hours.textContent = hours < 10 ? '0' + hours : hours;
+	  $minutes.textContent = minutes < 10 ? '0' + minutes : minutes;
+	  $seconds.textContent = seconds < 10 ? '0' + seconds : seconds;
+	  $days.dataset.title = declensionNum(days, ['день', 'дня', 'дней']);
+	  $hours.dataset.title = declensionNum(hours, ['час', 'часа', 'часов']);
+	  $minutes.dataset.title = declensionNum(minutes, ['минута', 'минуты', 'минут']);
+	  $seconds.dataset.title = declensionNum(seconds, ['секунда', 'секунды', 'секунд']);
+	}
+	// получаем элементы, содержащие компоненты даты
+	const $days = document.querySelector('.timer__days');
+	const $hours = document.querySelector('.timer__hours');
+	const $minutes = document.querySelector('.timer__minutes');
+	const $seconds = document.querySelector('.timer__seconds');
+	// вызываем функцию countdownTimer
+	countdownTimer();
+	// вызываем функцию countdownTimer каждую секунду
+	timerId = setInterval(countdownTimer, 1000);
+  });
